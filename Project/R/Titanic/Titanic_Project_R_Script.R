2d8de665c1ed86d045153926360cd445378163d5
David800823 <sierradavid23@hotmail.com>
2023-09-13 19:27:51 -0400

    New Changes to My Git

---
+++ Project/R/Titanic/Titanic_Project_R_Script.R
@@ -0,0 +1,38 @@
+install.packages("tidyverse")
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("janitor")
+install.packages("skimr")
+install.packages("here")
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(janitor)
+library(skimr)
+library(here)
+
+train <- read_csv("Dataset/train.csv")
+
+head(train)
+
+skim_without_charts(train)
+
+keep_columns = c("PassengerId", "Survived", "Pclass", "Age", "Sex")
+
+cleaned_train <- train[keep_columns]
+
+skim_without_charts(cleaned_train)
+
+cleaned_train %>% 
+  drop_na()
+
+ggplot(data = cleaned_train, aes(x = Sex,fill = Sex))+ 
+  geom_bar() + 
+  labs("text", title = "Total Count of Each Gender") +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 5.5, color = "white") +
+  theme_classic()
+
+
